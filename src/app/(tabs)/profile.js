be2cc9d8b46711bd17dd314daa461712d3b5bec9
Renan Cavichi <renancavichi@gmail.com>
2025-11-10 15:19:31 -0300

    tela de perfil e logout

--- src/app/(tabs)/profile.js
+++ src/app/(tabs)/profile.js
@@ -1,28 +1,30 @@
 import { View, Text, Button, StyleSheet } from 'react-native'
 import { useRouter } from 'expo-router'
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import { useAuthStore } from '../../stores/useAuthStore'
+import { Image } from 'expo-image'
 
 export default function Profile() {
 
     const router = useRouter()
+    const {profile, logout} = useAuthStore()
 
 
     const handleLogout = async () => {
-        await AsyncStorage.removeItem('logado')
+        await AsyncStorage.removeItem('userLogged')
+        logout()
         router.replace('/login')
     }
 
     return (
         <View style={styles.container}>
-            <Text>Página de Perfil</Text>
-            <Button 
-                title='Home'
-                onPress={() => router.navigate('/')}
+            <Image 
+                source={profile?.avatar || 'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y'} 
+                style={{width: 100, height: 100, borderRadius: 50, marginBottom: 20}} 
             />
-            <Button 
-                title='Sobre'
-                onPress={() => router.push('/about')}
-            />
+            <Text>{profile.name}</Text>
+            <Text>{profile.email}</Text>
+            
             <Button 
                 title='Logout'
                 onPress={handleLogout}
--- src/app/index.js
+++ src/app/index.js
@@ -1,36 +1,33 @@
-import { View, Text, Button, StyleSheet } from 'react-native'
+import { View, Text, Button, StyleSheet, ActivityIndicator } from 'react-native'
 import { useRouter } from 'expo-router'
 import { useEffect } from 'react'
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import { useAuthStore } from '../stores/useAuthStore'
 
 export default function Initializer() {
 
     const router = useRouter()
+    const { login } = useAuthStore()
 
     useEffect(() => {
         const checkLogin = async () => {
-            const isLogged = await AsyncStorage.getItem('logado')
-            if(isLogged === 'true'){
+            const userLoggedString = await AsyncStorage.getItem('userLogged')
+            const userLogged = userLoggedString ? JSON.parse(userLoggedString) : null
+            if(userLogged?.token){
+                login(userLogged)
                 router.replace('/home')
             } else {
                 router.replace('/login')
             }
         }
 
-        checkLogin()
+        setTimeout(checkLogin, 2000)
     }, [])
 
     return (
         <View style={styles.container}>
-            <Text>Inicializador</Text>
-            <Button 
-                title='Login'
-                onPress={() => router.navigate('/login')}
-            />
-            <Button 
-                title='Home'
-                onPress={() => router.navigate('/home')}
-            />
+            <Text>Meu Site</Text>
+            <ActivityIndicator size="large" color="#111111ff" />
         </View>
     )
 }
